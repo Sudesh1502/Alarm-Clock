acb1ebec04a130c1bb993fc98c0709ea92446e30
Sudesh Mhamankar <sudeshmhamankar0909@gmail.com>
2024-04-20 00:54:06 +0530

    js modified

--- app.js
+++ app.js
@@ -18,8 +18,7 @@ function ringSound(){
 setInterval(() => {
     let currentTime = new Date();
 
-hrs.textContent = (currentTime.getHours() < 10 ? "0" : "") + ((currentTime.getHours() % 12) || 12);
-
+    hrs.textContent = (currentTime.getHours() < 10 ? "0" : "") + (currentTime.getHours() > 12 ? currentTime.getHours() - 12 : currentTime.getHours()) ;
     min.textContent = (currentTime.getMinutes() < 10 ? "0" : "") + currentTime.getMinutes();
     sec.textContent = (currentTime.getSeconds() < 10 ? "0" : "") + currentTime.getSeconds();
 
